bf21d9537bcf46963ce2e2080b701c5f2cc30309
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-02-16 12:54:49 -0800

    created index.js & resolvers.js within new folder schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,8 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require('mongoose')
+const bcrypt = require('bcrypt')
 
 // import schema from Book.js
-const bookSchema = require('./Book');
+const bookSchema = require('./Book')
 
 const userSchema = new Schema(
   {
@@ -30,28 +30,28 @@ const userSchema = new Schema(
       virtuals: true,
     },
   }
-);
+)
 
 // hash user password
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
+    const saltRounds = 10
+    this.password = await bcrypt.hash(this.password, saltRounds)
   }
 
-  next();
-});
+  next()
+})
 
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
-};
+  return bcrypt.compare(password, this.password)
+}
 
 // when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
 userSchema.virtual('bookCount').get(function () {
-  return this.savedBooks.length;
-});
+  return this.savedBooks.length
+})
 
-const User = model('User', userSchema);
+const User = model('User', userSchema)
 
-module.exports = User;
+module.exports = User
